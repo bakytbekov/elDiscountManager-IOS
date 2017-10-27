9817c8a5e76173acc70cc28a6825b665e50ee0bd
ZYFAR <zyfar@ITLabAdmins-iMac-2.local>
2017-10-27 16:23:04 +0600

    add Project

---
+++ elDiscountManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  elDiscountManager
+//
+//  Created by ZYFAR on 27.10.17.
+//  Copyright © 2017 NeoBis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ elDiscountManagerTests/elDiscountManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  elDiscountManagerTests.swift
+//  elDiscountManagerTests
+//
+//  Created by ZYFAR on 27.10.17.
+//  Copyright © 2017 NeoBis. All rights reserved.
+//
+
+import XCTest
+@testable import elDiscountManager
+
+class elDiscountManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
